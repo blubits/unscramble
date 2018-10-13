a2eeb05524eb707a825159d5ea313e61af9de1b9
jetsalinas <joseenrico.salinas@gmail.com>
2018-10-13 17:31:22 +0800

    Added game.py file

---
+++ game.py
@@ -0,0 +1,14 @@
+"""
+Controller class for SWUG
+
+:Author:     Jose Enrico Salinas
+:Version:    v20181013
+"""
+
+class Game:
+
+    def __init__(self, dictionary_address='dictionary.txt'):
+        raise NotImplementedError
+
+    def run(self):
+        raise NotImplementedError
