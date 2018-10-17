112dbce656357190d160d1fbc04336fc5a88b472
Maded Batara III <mnbatara3@gmail.com>
2018-10-17 18:46:29 +0800

    test: removed list() casting

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -19,26 +19,31 @@ class Test_TestDictionary(unittest.TestCase):
     def test_choice(self):
         self.assertEqual(self.dictionary.choice() in self.dictionary, True)
         self.assertEqual(self.dictionary.choice() in self.dictionary, True)
-    
+
     def test_filter_by_anagram(self):
-        self.assertEqual(list(self.dictionary.filter_by_anagram("bar")), ["arb", "bar", "rab"])
+        self.assertEqual(self.dictionary.filter_by_anagram(
+            "bar"), ["arb", "bar", "rab"])
 
     def test_filter_by_length(self):
-        self.assertEqual(list(self.dictionary.filter_by_length(start=3)), ["arb", "bar", "foo", "rab", "ree"])
-        self.assertEqual(list(self.dictionary.filter_by_length(start=3, end=4)), ["arb", "bar", "baza", "foo", "rab", "ree"])
+        self.assertEqual(self.dictionary.filter_by_length(
+            start=3), ["arb", "bar", "foo", "rab", "ree"])
+        self.assertEqual(self.dictionary.filter_by_length(start=3, end=4), [
+                         "arb", "bar", "baza", "foo", "rab", "ree"])
 
     def test_filter_from_string(self):
-        self.assertEqual(list(self.dictionary.filter_from_string("arb")), ["arb", "bar", "rab"])
-        self.assertEqual(list(self.dictionary.filter_from_string("oof")), ["foo"])
+        self.assertEqual(self.dictionary.filter_from_string(
+            "arb"), ["arb", "bar", "rab"])
+        self.assertEqual(self.dictionary.filter_from_string("oof"), ["foo"])
 
     def test_group_by_length(self):
-        self.assertEqual(self.dictionary.group_by_length(), {3: ["arb", "bar", "foo", "rab", "ree"], 4: ["baza"]})
+        self.assertEqual(self.dictionary.group_by_length(), {
+                         3: ["arb", "bar", "foo", "rab", "ree"], 4: ["baza"]})
 
     def test_minimal_string(self):
         self.assertEqual(self.dictionary.minimal_string(), "aabeefoorz")
 
     def test_random(self):
-        for word in list(self.dictionary.random()):
+        for word in self.dictionary.random():
             self.assertEqual(word in self.dictionary, True)
 
 if __name__ == '__main__':
