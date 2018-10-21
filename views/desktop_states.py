264012c1c8fce71b229bb65b692c7da0efd4edd4
jetsalinas <joseenrico.salinas@gmail.com>
2018-10-21 16:36:35 +0800

    Added desktop states enum in preparation for gui implementation

---
+++ views/desktop_states.py
@@ -0,0 +1,15 @@
+"""
+Enum class for desktop states
+
+:Author:     Jose Enrico Salinas
+:Version:    v20181021
+"""
+
+from enum import Enum
+
+class DesktopStates(Enum):
+
+    intro = "DESKTOP.STATE.INTRO"
+    menu = "DESKTOP.STATE.MENU"
+    options = "DESKTOP.STATE.OPTIONS"
+    game = "DESKTOP.STATE.GAME"
