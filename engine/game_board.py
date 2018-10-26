54fe0695f67159055250e361000040d2db569257
Maded Batara III <mnbatara3@gmail.com>
2018-10-26 11:55:40 +0800

    feat: refactor game_board, game_modes, timer

--- engine/game_board.py
+++ engine/game_board.py
@@ -3,17 +3,16 @@ A game board (i.e. list of words).
 
 :Author:     Maded Batara III
 :Author:     Jose Enrico Salinas
-:Version:    v20181020
+:Version:    v20181024
 """
 
 from math import ceil
 
 class GameBoard:
 
-    def __init__(self, string, dictionary):
-        self.string = string
-        self.query = dictionary.filter_from_string(
-            string).filter_by_length(3, len(string))
+    def __init__(self, query, columns=7):
+        self.query = query
+        self.columns = columns
         self.board = {}
         for word in self.query:
             self.board[word] = False
@@ -25,9 +24,8 @@ class GameBoard:
         return len(self.query)
 
     def __str__(self):
-        columns = 7
-        words_per_column = ceil(len(self.query) / columns)
-        board = [[] for _ in range(columns)]
+        words_per_column = ceil(len(self.query) / self.columns)
+        board = [[] for _ in range(self.columns)]
         col = 0
         for _, words in sorted(self.query.group_by_length().items()):
             for word in words:
@@ -39,12 +37,11 @@ class GameBoard:
                 if len(board[col]) == words_per_column:
                     col += 1
         return '\n'.join(
-            ["String: {0}".format(self.string)] +
             [' '.join(row[i] if len(row) > i else '' for row in board)
              for i in range(words_per_column)]
         )
 
-    def answer(self, word):
+    def fill(self, word):
         if word in self.query:
             self.board[word] = True
             return True
