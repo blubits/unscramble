2d48284e217a7aec5403930bff2e7f712ed79e46
Maded Batara III <mnbatara3@gmail.com>
2018-11-02 15:44:59 +0800

    docs: game

--- engine/game.py
+++ engine/game.py
@@ -1,5 +1,6 @@
 """
 Game instance.
+
 :Author:     Maded Batara III
 :Version:    v20181024
 """
@@ -8,8 +9,28 @@ from .game_board import GameBoard
 from .helpers import *
 
 class Game:
+    """An instance of the Simple Word Unscrambler Game.
+
+    The Game class is meant to be a 'shell' for any game interface to
+    instantiate and manipulate if necessary. As such, it does not
+    change its own state unless a word is filled up or the maximum
+    number of retries have been reached.
+
+    Attributes:
+        current_score (int): Current score achieved by the player.
+        maximum_score (int): Maximum score achievable by a player.
+    """
 
     def __init__(self, query, *, retries=None):
+        """
+        Initializes a new Game.
+
+        Args:
+            query (DictionaryQuery): A list of words queried from the
+                dictionary
+            retries (int, optional): Number of retries (i.e. wrong answers)
+                before the game hits Game Over.
+        """
         self.board = GameBoard(query)
         self.current_score = 0
         self.maximum_score = sum(score(word) for word in self.board)
@@ -18,6 +39,7 @@ class Game:
 
     @property
     def is_game_over(self):
+        """bool: True if the game is over, False otherwise."""
         return self._is_game_over
 
     @is_game_over.setter
@@ -30,8 +52,19 @@ class Game:
             raise ValueError("game is already on game over")
 
     def answer(self, term):
+        """
+        Answers an item on the board.
+
+        Args:
+            term (str): Word to attempt to fill up on the board.
+
+        Returns:
+            bool: True if the word is on the board, False otherwise.
+        """
         if not self.is_game_over and not self.board.fill(term) and self.retries is not None:
             self.retries -= 1
             self.current_score += score(term)
+            return True
         if self.retries == 0:
             self.is_game_over = True
+        return False
