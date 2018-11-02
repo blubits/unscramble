60a1cf5ca8f2be0c11c291df3524c35f99973a16
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-02 21:33:37 +0800

    Added skeleton classes for desktop and terminal interfaces

---
+++ views/desktop.py
@@ -0,0 +1,14 @@
+"""
+A desktop interface for SWUG using pyglet
+
+:Author:     Jose Enrico Salinas
+:Version:    v20181102
+"""
+
+from .interface_handler import InterfaceHandler
+from .desktop_states import DesktopStates
+
+class DesktopInterface(InterfaceHandler):
+    
+    def __init__(self, game):
+        pass
---
+++ views/terminal.py
@@ -0,0 +1,13 @@
+"""
+A basic terminal interface.
+
+:Author:     Jose Enrico Salinas
+:Version:    v20181102
+"""
+
+from .interface_handler import InterfaceHandler
+
+class TerminalInterface(InterfaceHandler):
+    
+    def __init__(self, game):
+        pass
