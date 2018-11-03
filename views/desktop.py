a590b9001b725b35a1a8e8e36f694f912f3cf5b3
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-03 18:08:22 +0800

    Updated state functionality in desktop.py

--- views/desktop.py
+++ views/desktop.py
@@ -7,32 +7,43 @@ A desktop interface for SWUG using pyglet
 
 from .interface_handler import InterfaceHandler
 from .desktop_states import DesktopStates
+from .widgets import *
 
 import pyglet
+import glooey
 
 class DesktopInterface(InterfaceHandler):
 
     def __init__(self):
         self.state = DesktopStates.menu
         self.window = pyglet.window.Window(width=1200, height=900)
+        self.gui = glooey.Gui(self.window)
+
+        self.menu = MenuWidget()
+        self.gui.add(self.menu)
 
         @self.window.event
         def on_draw():
-            self.window.clear()
-
             if self.state == DesktopStates.intro:
                 #TODO: Implement intro display
-                print("In Intro")
+                pass
             elif self.state == DesktopStates.menu:
                 #TODO: Implement menu display
-                print("In Menu")
+                # self.draw_menu()
+                self.menu.do_draw()
             elif self.state == DesktopStates.options:
                 #TODO: Implement options display
                 print("In Options")
             elif self.state == DesktopStates.game:
                 #TODO: Implement game display
                 print("In game")
+            self.gui.on_draw()
+
 
     def run(self):
         pyglet.app.run()
         print("In DesktopInterface")
+
+    def draw_menu(self):
+        print("In Menu")
+        
