03f6dfcb21d51c0e46efcc693fc3c66e3757e94b
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-03 16:52:19 +0800

    Added state management to desktop interface

--- views/desktop.py
+++ views/desktop.py
@@ -9,13 +9,29 @@ from .interface_handler import InterfaceHandler
 from .desktop_states import DesktopStates
 
 import pyglet
-import glooey
 
 class DesktopInterface(InterfaceHandler):
 
     def __init__(self):
+        self.state = DesktopStates.menu
         self.window = pyglet.window.Window(width=1200, height=900)
-        self.gui = glooey.Gui(self.window)
+
+        @self.window.event
+        def on_draw():
+            self.window.clear()
+
+            if self.state == DesktopStates.intro:
+                #TODO: Implement intro display
+                print("In Intro")
+            elif self.state == DesktopStates.menu:
+                #TODO: Implement menu display
+                print("In Menu")
+            elif self.state == DesktopStates.options:
+                #TODO: Implement options display
+                print("In Options")
+            elif self.state == DesktopStates.game:
+                #TODO: Implement game display
+                print("In game")
 
     def run(self):
         pyglet.app.run()
