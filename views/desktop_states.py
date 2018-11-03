8500f242e7b9395a871f6bcd2ffda05b826c6352
Maded Batara III <mnbatara3@gmail.com>
2018-11-03 20:05:27 +0800

    cleaned up merge

---
+++ views/desktop_states.py
@@ -0,0 +1,14 @@
+"""
+Enum class for desktop states
+:Author:     Jose Enrico Salinas
+:Version:    v20181021
+"""
+
+from enum import Enum
+
+class DesktopStates(Enum):
+
+    intro = 0
+    menu = 1
+    options = 2
+game = 3
