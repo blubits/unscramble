18a287709d9732c469db02b8c3985147145719f7
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-03 19:31:50 +0800

    Implemented intro widget

--- views/desktop_widgets.py
+++ views/desktop_widgets.py
@@ -1,6 +1,15 @@
 import pyglet
 import glooey
 
+class IntroWidget(glooey.Widget):
+    
+    custom_alignment = 'center'
+
+    def __init__(self):
+        super().__init__()
+        title = TitleLabel("SWUG")
+        self._attach_child(title)
+
 class MenuWidget(glooey.Widget):
     
     custom_alignment = 'center'
@@ -39,3 +48,14 @@ class TitleLabel(MenuLabel):
     custom_font_size = 70
     custom_alignment = "center"
     custom_padding = 10
+
+class GameWidget(glooey.Widget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.vbox = glooey.VBox()
+        self.game_board_hbox = glooey.HBox()
+
+        self.vbox.add(self.game_board_hbox)
+        self._attach_child(self.vbox)
