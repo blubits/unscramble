e69646e403d2f7ddedc7b594b36a18ed36f4bf50
Maded Batara III <mnbatara3@gmail.com>
2018-11-03 17:23:57 +0800

    feat: add game_modes to engine

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,3 +1,5 @@
 from engine.dictionary import Dictionary
 from engine.dictionary_query import DictionaryQuery
 from engine.game_board import GameBoard
+from engine.game import Game
+from engine.game_modes import GameMode
