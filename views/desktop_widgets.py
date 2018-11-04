8a4a471e01b94fb619060711e4505ce140dca010
jetsalinas <joseenrico.salinas@gmail.com>
2018-11-04 13:10:21 +0800

    Added skeleton classes for game scene

--- views/desktop_widgets.py
+++ views/desktop_widgets.py
@@ -55,7 +55,7 @@ class GameWidget(glooey.Widget):
         super().__init__()
 
         self.vbox = glooey.VBox()
-        self.game_board_hbox = glooey.HBox()
+        self.game_board_hbox = GameBoardWidget()
         self.game_input = GameInputWidget()
         self.game_options = GameOptionsWidget()
 
@@ -63,6 +63,17 @@ class GameWidget(glooey.Widget):
         self.vbox.add(self.game_input)
         self.vbox.add(self.game_options)
         self._attach_child(self.vbox)
+
+class GameBoardWidget(glooey.Widget):
+
+    custom_alignment = "center"
+
+    def __init__(self):
+        super().__init__()
+
+        self.hbox = glooey.HBox()
+        self.hbox.add(MenuLabel("GameBoardWidget"))
+        self._attach_child(self.hbox)
 
 class GameInputWidget(glooey.Widget):
 
@@ -77,12 +88,59 @@ class GameInputWidget(glooey.Widget):
 
 class GameOptionsWidget(glooey.Widget):
 
-    custom_alignment = "center"
+    custom_alignment = "left"
 
     def __init__(self):
         super().__init__()
 
         self.hbox = glooey.HBox()
-        self.hbox.add(MenuLabel("GameOptionsWidget"))
+        self.hbox.add(GameInformationWidget())
+        self.hbox.add(GameButtonsWidget())
         self._attach_child(self.hbox)
 
+class GameInformationWidget(glooey.Widget):
+
+    custom_alignment = "left"
+    custom_padding = 30
+
+    def __init__(self):
+        super().__init__()
+
+        self.hbox = glooey.HBox()
+        self.hbox.add(InformationWidget("Time", "00:00"))
+        self.hbox.add(InformationWidget("Retries", "3"))
+        self.hbox.add(InformationWidget("Score", "0"))
+        self._attach_child(self.hbox)
+
+class InformationWidget(glooey.Widget):
+
+    custom_padding = 10
+
+    def __init__(self, label, information):
+        super().__init__()
+
+        self.vbox = glooey.VBox()
+        self.vbox.add(MenuLabel(label))
+        self.vbox.add(MenuLabel(information))
+        self._attach_child(self.vbox)
+
+class GameButtonsWidget(glooey.Widget):
+    custom_alignment = "right"
+
+    def __init__(self):
+        super().__init__()
+
+        self.hbox = glooey.HBox()
+
+        self.check_ans_button = GameButton("A")
+        self.hbox.add(self.check_ans_button)
+        self._attach_child(self.hbox)
+
+class GameButton(glooey.Button):
+    
+    class Label(glooey.Label):
+        custom_padding = 30
+        custom_color = "#000000"
+
+    class Base(glooey.Background):
+        custom_color = "#aaaaaa"
